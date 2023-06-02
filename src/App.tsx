73bb55c0dd4a9a30ab1ba5bb35180000a40a8df2
Shayirbek Abdymomunov <legionera803@gmail.com>
2023-06-02 17:19:53 +0600

    homework is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,94 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Ingredient from "./components/Ingredient/Ingredient";
 import meatImage from "./assets/meat.png";
-import {nanoid} from "nanoid";
+import { nanoid } from "nanoid";
 import cheeseImage from "./assets/cheese.png";
 import baconImage from "./assets/becon.png";
 import saladImage from "./assets/salad.png";
 import Burger from "./components/Burger/Burger";
-import './App.css';
+import "./App.css";
 
 type Ingredient = {
-    name: string,
-    price: number,
-    image: string,
-    id: string,
+  name: string;
+  price: number;
+  image: string;
+  id: string;
 };
 
 const INGREDIENTS: Ingredient[] = [
-    {name: 'Meat', price: 80, image: meatImage, id: nanoid()},
-    {name: 'Cheese', price: 50, image: cheeseImage, id: nanoid()},
-    {name: 'Bacon', price: 60, image: baconImage, id: nanoid()},
-    {name: 'Salad', price: 10, image: saladImage, id: nanoid()},
+  { name: "Meat", price: 80, image: meatImage, id: nanoid() },
+  { name: "Cheese", price: 50, image: cheeseImage, id: nanoid() },
+  { name: "Bacon", price: 60, image: baconImage, id: nanoid() },
+  { name: "Salad", price: 10, image: saladImage, id: nanoid() },
 ];
 
 const App = () => {
+  type Count = {
+    name: string;
+    count: number;
+  };
 
-    type Count = {
-        name: string,
-        count: number,
-    };
+  const [ingredientsUseState, setIngredientsUseState] = useState<Count[]>([
+    { name: "Meat", count: 0 },
+    { name: "Cheese", count: 0 },
+    { name: "Bacon", count: 0 },
+    { name: "Salad", count: 0 },
+  ]);
 
-    const [ingredientsUseState, setIngredientsUseState] = useState<Count[]>([
-        {name: "Meat", count: 0},
-        {name: "Cheese", count: 0},
-        {name: "Bacon", count: 0},
-        {name: "Salad", count: 0},
-    ]);
+  const [priceCount, setPiceCount] = useState(30);
 
-    const [priceCount, setPiceCount] = useState(30);
-
-    const countPlus = (name: string) => {
-        setIngredientsUseState(prevState => {
-            return prevState.map((thing, index) => {
-                if (thing.name === name) {
-                    setPiceCount(prevState => prevState + INGREDIENTS[index].price);
-                    return {
-                        ...thing,
-                        count: thing.count + 1,
-                    };
-                }
-                return thing
-            });
-        });
-    };
-
-    const countMinus = (name: string) => {
-        setIngredientsUseState(prevState => {
-            return prevState.map((thing, index) => {
-                if (thing.name === name) {
-                    if (thing.count > 0) {
-                        setPiceCount(prevState => prevState - INGREDIENTS[index].price);
-                        return {
-                            ...thing,
-                            count: thing.count - 1,
-                        };
-                    }
-                }
-                return thing
-            });
-        });
-    };
-
-
-    const showIngredients = INGREDIENTS.map((every, index) => {
-
-        return <Ingredient name={every.name}
-                           price={every.price}
-                           image={every.image}
-                           key={every.id}
-                           count={ingredientsUseState[index].count}
-                           plusClick={countPlus}
-                           minusClick={countMinus}
-        />
+  const countPlus = (name: string) => {
+    setIngredientsUseState((prevState) => {
+      return prevState.map((thing, index) => {
+        if (thing.name === name) {
+          setPiceCount((prevState) => prevState + INGREDIENTS[index].price);
+          return {
+            ...thing,
+            count: thing.count + 1,
+          };
+        }
+        return thing;
+      });
     });
+  };
 
+  const countMinus = (name: string) => {
+    setIngredientsUseState((prevState) => {
+      return prevState.map((thing, index) => {
+        if (thing.name === name) {
+          if (thing.count > 0) {
+            setPiceCount((prevState) => prevState - INGREDIENTS[index].price);
+            return {
+              ...thing,
+              count: thing.count - 1,
+            };
+          }
+        }
+        return thing;
+      });
+    });
+  };
+
+  const showIngredients = INGREDIENTS.map((every, index) => {
     return (
-        <div className="App">
-            {showIngredients}
-            <Burger price={priceCount}
-                    countState={ingredientsUseState}
-            />
-        </div>
+      <Ingredient
+        name={every.name}
+        price={every.price}
+        image={every.image}
+        key={every.id}
+        count={ingredientsUseState[index].count}
+        plusClick={countPlus}
+        minusClick={countMinus}
+      />
     );
+  });
+
+  return (
+    <div className="App">
+      {showIngredients}
+      <Burger price={priceCount} countState={ingredientsUseState} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,38 +1,35 @@
-import React from 'react';
+import React from "react";
 
 type Count = {
-    name: string,
-    count: number,
+  name: string;
+  count: number;
 };
 interface IBurger {
-    price: number,
-    countState: Count[],
+  price: number;
+  countState: Count[];
 }
-const Burger: React.FC<IBurger> = props => {
-
-    return (
-            <div className="burgerBlock">
-                <div className="Burger">
-                    <div className="BreadTop">
-                        <div className="Seeds1"></div>
-                        <div className="Seeds2"></div>
-                    </div>
-                    {
-                        props.countState.map(every => {
-                            const ingsArr : React.ReactNode[] = [];
-                            for (let i = 0; i < every.count; i++) {
-                                ingsArr.push(<div className={every.name}></div>);
-                            }
-                            return ingsArr;
-                        })
-                    }
-                    <div className="BreadBottom"></div>
-                </div>
-                <p>
-                    <strong>Price: {props.price} KGS</strong>
-                </p>
-            </div>
-    );
+const Burger: React.FC<IBurger> = (props) => {
+  return (
+    <div className="burgerBlock">
+      <div className="Burger">
+        <div className="BreadTop">
+          <div className="Seeds1"></div>
+          <div className="Seeds2"></div>
+        </div>
+        {props.countState.map((every) => {
+          const ingsArr: React.ReactNode[] = [];
+          for (let i = 0; i < every.count; i++) {
+            ingsArr.push(<div className={every.name}></div>);
+          }
+          return ingsArr;
+        })}
+        <div className="BreadBottom"></div>
+      </div>
+      <p>
+        <strong>Price: {props.price} KGS</strong>
+      </p>
+    </div>
+  );
 };
 
 export default Burger;
